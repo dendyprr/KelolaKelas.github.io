046a2390484288c5facdccb615262dcfbd649028
dendy <dendyprr07@gmail.com>
2026-04-21 00:33:37 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,13 +5,13 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">	
 	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-	<link rel="stylesheet" type="text/css" href="{{asset('login-form/login_v1/vendor/bootstrap/css/bootstrap.min.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{asset('login-form/login_v1/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{asset('login-form/login_v1/vendor/animate/animate.css')}}">	
-	<link rel="stylesheet" type="text/css" href="{{asset('login-form/login_v1/vendor/css-hamburgers/hamburgers.min.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{asset('login-form/login_v1/vendor/select2/select2.min.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{asset('login-form/login_v1/css/util.css')}}">
-	<link rel="stylesheet" type="text/css" href="{{asset('login-form/login_v1/css/main.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('login-form/Login_v1/vendor/bootstrap/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('login-form/Login_v1/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('login-form/Login_v1/vendor/animate/animate.css')}}">	
+	<link rel="stylesheet" type="text/css" href="{{asset('login-form/Login_v1/vendor/css-hamburgers/hamburgers.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('login-form/Login_v1/vendor/select2/select2.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('login-form/Login_v1/css/util.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('login-form/Login_v1/css/main.css')}}">
 </head>
 <body>
 	
@@ -19,7 +19,7 @@
 		<div class="container-login100">
 			<div class="wrap-login100">
 				<div class="login100-pic js-tilt" data-tilt>
-					<img src="{{asset('login-form/login_v1/images/img-01.png')}}" alt="IMG">
+					<img src="{{asset('login-form/Login_v1/images/img-01.png')}}" alt="IMG">
 				</div>
 
 				<form action="{{route('auth-login-proccess')}}" class="login100-form validate-form" autocomplete="off">
@@ -78,17 +78,17 @@
 	
 
 		
-	<script src="{{asset('login-form/login_v1/vendor/jquery/jquery-3.2.1.min.js')}}"></script>
-	<script src="{{asset('login-form/login_v1/vendor/bootstrap/js/popper.js')}}"></script>
-	<script src="{{asset('login-form/login_v1/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
-	<script src="{{asset('login-form/login_v1/vendor/select2/select2.min.js')}}"></script>
-	<script src="{{asset('login-form/login_v1/vendor/tilt/tilt.jquery.min.js')}}"></script>
+	<script src="{{asset('login-form/Login_v1/vendor/jquery/jquery-3.2.1.min.js')}}"></script>
+	<script src="{{asset('login-form/Login_v1/vendor/bootstrap/js/popper.js')}}"></script>
+	<script src="{{asset('login-form/Login_v1/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+	<script src="{{asset('login-form/Login_v1/vendor/select2/select2.min.js')}}"></script>
+	<script src="{{asset('login-form/Login_v1/vendor/tilt/tilt.jquery.min.js')}}"></script>
 	<script >
 		$('.js-tilt').tilt({
 			scale: 1.1
 		})
 	</script>
-	<script src="{{asset('login-form/login_v1/js/main.js')}}"></script>
+	<script src="{{asset('login-form/Login_v1/js/main.js')}}"></script>
 
     @if(session('success'))
         <script>
