19458be930dab598e8d9d30fda3992cdefbc2f68
dendy <dendyprr07@gmail.com>
2026-04-21 00:22:47 +0700

    update

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,13 +5,13 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">	
 	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
-	<link rel="stylesheet" type="text/css" href=" {{asset('login-form/login_v1/vendor/bootstrap/css/bootstrap.min.css')}} ">
-	<link rel="stylesheet" type="text/css" href=" {{asset('login-form/login_v1/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}} ">
-	<link rel="stylesheet" type="text/css" href=" {{asset('login-form/login_v1/vendor/animate/animate.css')}} ">	
-	<link rel="stylesheet" type="text/css" href=" {{asset('login-form/login_v1/vendor/css-hamburgers/hamburgers.min.css')}} ">
-	<link rel="stylesheet" type="text/css" href=" {{asset('login-form/login_v1/vendor/select2/select2.min.css')}} ">
-	<link rel="stylesheet" type="text/css" href=" {{asset('login-form/login_v1/css/util.css')}} ">
-	<link rel="stylesheet" type="text/css" href=" {{asset('login-form/login_v1/css/main.css')}} ">
+	<link rel="stylesheet" type="text/css" href="{{asset('login-form/login_v1/vendor/bootstrap/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('login-form/login_v1/fonts/font-awesome-4.7.0/css/font-awesome.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('login-form/login_v1/vendor/animate/animate.css')}}">	
+	<link rel="stylesheet" type="text/css" href="{{asset('login-form/login_v1/vendor/css-hamburgers/hamburgers.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('login-form/login_v1/vendor/select2/select2.min.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('login-form/login_v1/css/util.css')}}">
+	<link rel="stylesheet" type="text/css" href="{{asset('login-form/login_v1/css/main.css')}}">
 </head>
 <body>
 	
@@ -78,11 +78,11 @@
 	
 
 		
-	<script src=" {{asset('login-form/login_v1/vendor/jquery/jquery-3.2.1.min.js')}} "></script>
-	<script src=" {{asset('login-form/login_v1/vendor/bootstrap/js/popper.js')}} "></script>
-	<script src=" {{asset('login-form/login_v1/vendor/bootstrap/js/bootstrap.min.js')}} "></script>
-	<script src=" {{asset('login-form/login_v1/vendor/select2/select2.min.js')}} "></script>
-	<script src=" {{asset('login-form/login_v1/vendor/tilt/tilt.jquery.min.js')}} "></script>
+	<script src="{{asset('login-form/login_v1/vendor/jquery/jquery-3.2.1.min.js')}}"></script>
+	<script src="{{asset('login-form/login_v1/vendor/bootstrap/js/popper.js')}}"></script>
+	<script src="{{asset('login-form/login_v1/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+	<script src="{{asset('login-form/login_v1/vendor/select2/select2.min.js')}}"></script>
+	<script src="{{asset('login-form/login_v1/vendor/tilt/tilt.jquery.min.js')}}"></script>
 	<script >
 		$('.js-tilt').tilt({
 			scale: 1.1
